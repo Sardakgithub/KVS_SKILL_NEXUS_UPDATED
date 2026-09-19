d0956ecd4bcfa5170966d6337e854769f8adef13
sardak <sardakpadala@gmail.com>
2026-09-19 12:52:37 +0530

    last commit

--- config/settings/base.py
+++ config/settings/base.py
@@ -182,8 +182,9 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 STORAGES = {
     "default": {"BACKEND": "django.core.files.storage.FileSystemStorage"},
-    "staticfiles": {"BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage"},
+    "staticfiles": {"BACKEND": "whitenoise.storage.CompressedStaticFilesStorage"},
 }
+WHITENOISE_MANIFEST_STRICT = False
 
 # --------------------------------------------------------------------------
 # Django REST Framework
